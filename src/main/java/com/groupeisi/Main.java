1d2adbaa31d104b7af44f78a12937509c79f2801
Boubacar Siddy Diallo <dialloboubacarsiddy2004@gmail.com>
2023-11-17 13:31:27 +0000

    dynamic instantiation

--- src/main/java/com/groupeisi/Main.java
+++ src/main/java/com/groupeisi/Main.java
@@ -1,13 +1,27 @@
 package com.groupeisi;
 
+import java.util.Scanner;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Context context = new Context();
         context.setiStrategy(new StrategyImpl1());
         context.effectuerOperation();
         context.setiStrategy(new StrategyImpl2());
         context.effectuerOperation();
+
+        // 2ème méthode :
+        Scanner scanner = new Scanner(System.in);
+        while(true){
+            System.out.println("Quelle strategie ? ");
+            String className = scanner.nextLine();
+            IStrategy iStrategy = (IStrategy) Class.forName(className).getConstructor().newInstance();
+            context.setiStrategy(iStrategy);
+            context.effectuerOperation();
+        }
     }
+
+    // Raison d'utilisation : Quand un objet doit pouvoir faire varier une partie de son algorithme dynamiquement
 }
